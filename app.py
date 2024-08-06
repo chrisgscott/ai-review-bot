1efc9657cd946fd86b991019f5c9a932db0dfc75
chrisgscott <chrisgscott@gmail.com>
2024-08-05 20:39:27 -0700

    Deleted first name from the user model because I'm a dumbass.

--- app.py
+++ app.py
@@ -80,7 +80,6 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    first_name = db.Column(db.String(50), nullable=True)
     is_admin = db.Column(db.Boolean, default=False)
     registration_date = db.Column(db.DateTime, default=datetime.utcnow)
     last_login = db.Column(db.DateTime)
